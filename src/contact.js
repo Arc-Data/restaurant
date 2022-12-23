b01ca9aa7c19417da9f12d159d072a7e3a8fabac
Arc-Data <arclight04@gmail.com>
2022-12-23 14:21:40 +0800

    added contact and menu page content

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,51 @@
-import resetContent from './removeChild'
+import resetContent from './removeChild';
+import phone from './assets/phone-solid.svg';
+import location from './assets/location-pin-solid.svg';
+import clock from './assets/clock-solid.svg';
 
 const contact = (() => {
 	const container = document.querySelector('#content');
 	
 	const renderContactSection = () => {
-		console.log("Contact Section");
+		const contactContainer = document.createElement('div');
+		contactContainer.classList.add('contact-container');
+		container.appendChild(contactContainer);
+
+		const h2 = document.createElement('h2');
+		h2.textContent = 'Contact Us';
+		h2.classList.add('contact-title');
+		contactContainer.appendChild(h2);
+
+		const contactCard = document.createElement('div');
+		contactCard.classList.add('contact-card');
+		contactContainer.appendChild(contactCard);
+
+		const phoneImg = document.createElement('img');
+		phoneImg.src = phone;
+		phoneImg.classList.add('icon');
+		contactCard.appendChild(phoneImg);
+
+		const phoneText = document.createElement('p')
+		phoneText.textContent = '0999-999-9999';
+		contactCard.appendChild(phoneText);
+
+		const locationImg = document.createElement('img');
+		locationImg.src = location;
+		locationImg.classList.add('icon');
+		contactCard.appendChild(locationImg);
+
+		const locationText = document.createElement('p')
+		locationText.textContent = '123 Somewhere St., Unkown City, Country'
+		contactCard.appendChild(locationText);
+
+		const timeImg = document.createElement('img');
+		timeImg.src = clock;
+		timeImg.classList.add('icon');
+		contactCard.appendChild(timeImg);
+
+		const timeText = document.createElement('p')
+		timeText.textContent = 'Open 24/7'
+		contactCard.appendChild(timeText);
 	}
 
 	const render = () => {
--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,96 @@
 import resetContent from './removeChild';
+import steak from './assets/steak.jpg';
+import salad from './assets/salad.jpg';
 
 const menu = (() => {
 	
 	const container = document.querySelector('#content');
 
+	const steakSelection = menuContainer => {
+
+		const steakContainer = document.createElement('div');
+		steakContainer.classList.add('menu-item-container');
+		menuContainer.appendChild(steakContainer);
+
+		const steakImg = document.createElement('img');
+		steakImg.src = steak;
+		steakImg.classList.add('menu-img');
+		steakContainer.appendChild(steakImg);
+
+		const steakSelection = document.createElement('div');
+		steakSelection.classList.add('menu-list');
+		steakContainer.appendChild(steakSelection);
+
+		const steakTitle = document.createElement('p');
+		steakTitle.classList.add('menu-selection-title');
+		steakTitle.textContent = 'Steak';
+		steakSelection.appendChild(steakTitle);
+
+		const steakItem1 = document.createElement('p');
+		steakItem1.textContent = 'Striploin';
+		steakSelection.appendChild(steakItem1);
+
+		const steakItem2 = document.createElement('p');
+		steakItem2.textContent = 'Tenderloin';
+		steakSelection.appendChild(steakItem2);
+
+		const steakItem3 = document.createElement('p');
+		steakItem3.textContent = 'Ribeye';
+		steakSelection.appendChild(steakItem3);
+
+		const steakItem4 = document.createElement('p');
+		steakItem4.textContent = 'T-Bone';
+		steakSelection.appendChild(steakItem4);
+	}
+
+	const saladSelection = menuContainer => {
+		const saladContainer = document.createElement('div');
+		saladContainer.classList.add('menu-item-container');
+		menuContainer.appendChild(saladContainer);
+
+		const saladImg = document.createElement('img');
+		saladImg.src = salad;
+		saladImg.classList.add('menu-img');
+		saladContainer.appendChild(saladImg);
+
+		const saladSelection = document.createElement('div');
+		saladSelection.classList.add('menu-list');
+		saladContainer.appendChild(saladSelection);
+
+		const saladTitle = document.createElement('p');
+		saladTitle.classList.add('menu-selection-title');
+		saladTitle.textContent = 'Salad';
+		saladSelection.appendChild(saladTitle);
+
+		const saladItem1 = document.createElement('p');
+		saladItem1.textContent = 'Caesar Salad';
+		saladSelection.appendChild(saladItem1);
+
+		const saladItem2 = document.createElement('p');
+		saladItem2.textContent = 'Greek Salad';
+		saladSelection.appendChild(saladItem2);
+
+		const saladItem3 = document.createElement('p');
+		saladItem3.textContent = 'Sweet Potato Salad';
+		saladSelection.appendChild(saladItem3);
+
+		const saladItem4 = document.createElement('p');
+		saladItem4.textContent = 'Fish Caesar';
+		saladSelection.appendChild(saladItem4);
+	}
+
 	const renderMenuSection = () => {
-		console.log("Menu Section");
+		const menuContainer = document.createElement('div');
+		menuContainer.classList.add('menu-container');
+		container.appendChild(menuContainer);
+
+		const h2 = document.createElement('h2');
+		h2.textContent = 'Our Menu';
+		h2.classList.add('menu-title');
+		menuContainer.appendChild(h2);
+
+		steakSelection(menuContainer);
+		saladSelection(menuContainer);
 	};
 
 	const render = () => {
